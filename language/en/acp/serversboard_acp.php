6b23d3ca9e9ebbb424900dc3e7f4f3c00a244cee
Token07 <token@corenetwork.us>
2016-03-09 13:48:00 -0800

    Edit -> Add

--- language/en/acp/serversboard_acp.php
+++ language/en/acp/serversboard_acp.php
@@ -20,7 +20,7 @@ if (empty($lang) || !is_array($lang))
 
 $lang = array_merge($lang, array(
 	'TOKEN07_SERVERSBOARD_ACP_SERVERSBOARD'	=> 'Servers Board',
-	'TOKEN07_SERVERSBOARD_ACP_EDIT'			=> 'Edit Servers',
+	'TOKEN07_SERVERSBOARD_ACP_ADD'			=> 'Add Servers',
 	'TOKEN07_SERVERSBOARD_ACP_DISPINDEX'	=> 'Display servers board on index',
 	'TOKEN07_SERVERSBOARD_ACP_UPDATEFREQ'	=> 'Server data update frequency (seconds)',
 	'TOKEN07_SERVERSBOARD_ACP_SERVERIP'		=> 'Server IP (in IP:Port format)',
